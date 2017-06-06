98ba2ee4e63300cf57e93a7cfa19f34d893509a5
aliface <balashovaalice@gmail.com>
2017-06-06 10:58:13 -0400

    Create RegisterActivity.java

---
+++ RegisterActivity.java
@@ -0,0 +1,18 @@
+package com.example.homepagemod;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    static String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG, "Yay it works!");
+
+    }
+}
