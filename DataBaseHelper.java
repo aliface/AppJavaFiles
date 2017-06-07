71aa51773487d243bce5c19a19c84fcce8ecf08c
aliface <balashovaalice@gmail.com>
2017-06-07 11:35:52 -0400

    Update DataBaseHelper.java

--- DataBaseHelper.java
+++ DataBaseHelper.java
@@ -18,15 +18,15 @@ public class DataBaseHelper extends SQLiteOpenHelper{
     private static final String TAG = "DataBaseHelper";
 
     private static final int DATABASE_VERSION = 1;
-    private static final String DATABASE_NAME = "ContactInfo.db"; //name of the database
-    private static final String TABLE_NAME = "contact_table";           // col 1
-    private static final String COLUMN_ID = "id";                       // col 2
-    private static final String COLUMN_NAME = "Full Name";              // col 3
-    private static final String COLUMN_USERNAME = "Username";           // col 4
-    private static final String COLUMN_PASSWORD = "Password";           // col 5
-    private static final String COLUMN_CARTYPE = "Car Type";            // col 6
-    private static final String COLUMN_LICENSEPLATE = "License Plate";  // col 7
-    private static final String COLUMN_EMAIL = "Email";                 // col 8
+    private static final String DATABASE_NAME = "rucharged.db"; //name of the database
+    private static final String TABLE_NAME = "users";
+    private static final String COLUMN_ID = "id";                       // col 1
+    private static final String COLUMN_NAME = "name";                   // col 2
+    private static final String COLUMN_USERNAME = "username";           // col 3
+    private static final String COLUMN_PASSWORD = "password";           // col 4
+    private static final String COLUMN_CARTYPE = "cartype";             // col 5
+    private static final String COLUMN_LICENSEPLATE = "licenseplate";   // col 6
+    private static final String COLUMN_EMAIL = "email";                 // col 7
 
     SQLiteDatabase db;
 
@@ -37,7 +37,14 @@ public class DataBaseHelper extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase db){
-        String createTable = "CREATE TABLE" + TABLE_NAME +"(ID INTEGER PRIMARY KEY AUTOINCREMENT)" + COLUMN_ID +"TEXT"; //query that we create to execute
+        String createTable = "CREATE TABLE" + TABLE_NAME +"(ID INTEGER PRIMARY KEY AUTOINCREMENT)"
+                + COLUMN_ID +"TEXT" //query that we create to execute
+                + COLUMN_NAME +"TEXT"
+                + COLUMN_USERNAME +"TEXT"
+                + COLUMN_PASSWORD +"TEXT"
+                + COLUMN_CARTYPE +"TEXT"
+                + COLUMN_LICENSEPLATE +"TEXT"
+                + COLUMN_EMAIL +"TEXT";
 
         db.execSQL(createTable); // executes the query you pass inside this method. Our query is createTable
         this.db = db;
@@ -49,25 +56,53 @@ public class DataBaseHelper extends SQLiteOpenHelper{
         onCreate(db);
     }
 
+    //Storing the details into the database
 
-    public boolean addData(String item){
+    public boolean addData(String name, String username, String password, String CarType, String LicensePlate, String email){
         SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(COLUMN_ID, item);
 
-        Log.d(TAG, "addDate: Adding " + item + "to" + TABLE_NAME);
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NAME, name);
+        values.put(COLUMN_USERNAME, username);
+        values.put(COLUMN_PASSWORD, password);
+        values.put(COLUMN_CARTYPE, CarType);
+        values.put(COLUMN_LICENSEPLATE, LicensePlate);
+        values.put(COLUMN_EMAIL, email);
 
-        long result = db.insert(TABLE_NAME, null, contentValues);
+        long result = db.insert(TABLE_NAME, null, values);
+
+        Log.d(TAG, "addDate: Adding to" + TABLE_NAME + result);
 
         //If the data is inserted incorrectly it will return the following
         if(result == -1) {
             return false;
         }else{
             return true;
-
         }
 
     }
+
+    public boolean getUser(String username, String password){
+        //HashMap<String, String> user = new HashMap<String, String>();
+
+        String selectQuery = "select + from " + TABLE_NAME + "where" + COLUMN_USERNAME + "="
+                + "'" + username + "'" + " and where" + COLUMN_PASSWORD + "=" + "'" + password +"'";
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        //Move the cursor to the first row
+        cursor.moveToFirst();
+        if(cursor.getCount()>0){
+            //user.put("email", cursor.getString(1));
+            return true;
+        }
+
+        cursor.close();
+        db.close();
+        return false;
+    }
+
 
 
 
@@ -91,7 +126,7 @@ public class DataBaseHelper extends SQLiteOpenHelper{
 
 
 
-    public void insertContact(Contact c ){
+    /*public void insertContact(Contact c ){
 
         db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
@@ -132,7 +167,7 @@ public class DataBaseHelper extends SQLiteOpenHelper{
             while(cursor.moveToNext());
         }
         return password;
-    }
+    }*/
 
 
 
